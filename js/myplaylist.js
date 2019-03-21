cbca7dbbd8503f9260fadd216fb6e9a24a74a981
DiogoAvilar <chorofutebolistico@hotmail.com>
2019-03-21 16:49:15 -0300

    Add files via upload

---
+++ js/myplaylist.js
@@ -0,0 +1,49 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/do re mi ft Gucci Mane.mp3',
+        title:'Do Re Mi',
+        artist:'Blacbear',
+        duration:'3:56',
+        cover:'mix/digitaldruglord.png'
+    },
+
+    {
+        mp3:'mix/Chateau.mp3',
+        title:'Chateau',
+        artist:'Blackbear',
+        duration:'3:31',
+        cover:'mix/digitaldruglord.png'
+    },
+    {
+        mp3:'mix/bright pink tims ft Cam ron Lyrics Lyric Video.mp3',
+        title:'Bright Pink Tims',
+        artist:'Blackbear',
+        duration:'3:34',
+        cover:'mix/cibersex.png'
+    },
+     {
+        mp3:'mix/anxiety blackbear ft FRND lyrics.mp3',
+        title:'Anxiety',
+        artist:'Blackbear',
+        duration:'3:14',
+        cover:'mix/cibersex.png'
+    },
+    {
+        mp3:'mix/Wanderlust OFFICIAL VERSION.mp3',
+        title:'Wanderlust',
+        artist:'Blackbear',
+        duration:'3:04',
+        cover:'mix/cashmerenoose.png'
+    }
+
+
+];
